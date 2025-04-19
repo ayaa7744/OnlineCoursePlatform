2bcaf5ff25e8793fb105cb5d21649dfcb76f7cfb
ayaa7744 <ayaashraf72003@gmail.com>
2025-04-19 20:21:34 +0200

    Update Student.cs

--- Data/Student.cs
+++ Data/Student.cs
@@ -2,9 +2,9 @@
 {
     public class Student
     {
-        public int Id { get; set; }  // المفتاح الأساسي
+        public int Id { get; set; }  
         public string Name { get; set; }
         public string Email { get; set; }
-        public ICollection<Course> Courses { get; set; } // علاقات مع الكورسات (ممكن يكون طالب في أكتر من كورس)
+        public ICollection<Course> Courses { get; set; } 
     }
 }
